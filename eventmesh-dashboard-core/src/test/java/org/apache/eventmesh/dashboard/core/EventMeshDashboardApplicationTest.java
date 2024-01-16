8ad765b345390a809d5bff8c00c1db85c1aac521
Pil0tXia <xiatian@apache.org>
2024-01-16 15:29:56 +0800

    Remove failed empty test

--- eventmesh-dashboard-core/src/test/java/org/apache/eventmesh/dashboard/core/EventMeshDashboardApplicationTest.java
+++ eventmesh-dashboard-core/src/test/java/org/apache/eventmesh/dashboard/core/EventMeshDashboardApplicationTest.java
@@ -1,13 +1,12 @@
 package org.apache.eventmesh.dashboard.core;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EventMeshDashboardApplicationTest {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
