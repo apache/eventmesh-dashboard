92b2953cbcf79a942c24aaee248de734ba459546
zzx <142965157+zzxxiansheng@users.noreply.github.com>
2024-02-05 17:39:53 +0800

    Update and rename EventmeshConsoleApplication.java to EventMeshDashboardApplication.java

--- eventmesh-dashboard-console/src/main/java/org/apache/eventmesh/dashboard/console/EventMeshDashboardApplication.java
+++ eventmesh-dashboard-console/src/main/java/org/apache/eventmesh/dashboard/console/EventMeshDashboardApplication.java
@@ -26,7 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @SpringBootApplication
 @EnableTransactionManagement
-public class EventmeshConsoleApplication {
+public class EventMeshDashboardApplication {
 
     public static void main(String[] args) {
         try {
