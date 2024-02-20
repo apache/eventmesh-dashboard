21a339cfd4a899406f50db4989ef682a2485f2c1
lambert@arch <lambertrao@outlook.com>
2024-02-20 15:22:16 +0800

    chore: rename FunctionManagerBean to FunctionManagerLoader

--- eventmesh-dashboard-console/src/main/java/org/apache/eventmesh/dashboard/console/spring/support/FunctionManagerLoader.java
+++ eventmesh-dashboard-console/src/main/java/org/apache/eventmesh/dashboard/console/spring/support/FunctionManagerLoader.java
@@ -27,7 +27,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class FunctionManagerBean {
+public class FunctionManagerLoader {
 
     private FunctionManager functionManager;
 
