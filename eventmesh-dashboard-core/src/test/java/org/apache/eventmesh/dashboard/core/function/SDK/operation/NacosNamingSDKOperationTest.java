45c1bf38c710767cb3bd089523dda7fc4cb1cdc4
lambert@arch <lambertrao@outlook.com>
2024-04-12 15:40:37 +0800

    feat: add Nacos Runtime Remoting service and its implementation. this service can get runtime info from nacos

---
+++ eventmesh-dashboard-core/src/test/java/org/apache/eventmesh/dashboard/core/function/SDK/operation/NacosNamingSDKOperationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.dashboard.core.function.SDK.operation;
+
+import org.apache.eventmesh.dashboard.core.function.SDK.SDKManager;
+import org.apache.eventmesh.dashboard.core.function.SDK.SDKTypeEnum;
+import org.apache.eventmesh.dashboard.core.function.SDK.config.CreateNacosConfig;
+
+import org.junit.jupiter.api.Test;
+
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+
+class NacosNamingSDKOperationTest {
+
+    private NacosNamingSDKOperation nacosNamingSDKOperation = new NacosNamingSDKOperation();
+
+    @Test
+    public void testCreateNamingService() throws NacosException, InterruptedException {
+        CreateNacosConfig createClientConfig = new CreateNacosConfig();
+        createClientConfig.setServerAddress("175.27.155.139:8848");
+        NamingService namingService = (NamingService) SDKManager.getInstance().createClient(SDKTypeEnum.META_NACOS_NAMING, createClientConfig)
+            .getValue();
+        namingService.registerInstance("eventmesh-dashboard-sdk-nacos-test", "11.11.11.11", 8888, "eventmesh-dashboard-sdk-nacos-test-cluster-name");
+
+        namingService.deregisterInstance("eventmesh-dashboard-sdk-nacos-test", "11.11.11.11", 8888,
+            "eventmesh-dashboard-sdk-nacos-test-cluster-name");
+    }
+}
