e38ef26826ae9b066ad71432fe82f1b81b16ae34
jie@apache.org <2732554140@qq.com>
2025-08-18 09:11:00 +0800

    fix(*):1. 保存更新，因为 rocketmq-exporter 官方文档 与 rocketmq-exporter 源码文档不一致，需要安装源码文档定义数据对象

---
+++ eventmesh-dashboard-console/src/main/java/org/apache/eventmesh/dashboard/console/function/report/collect/MetadataDataManage.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.eventmesh.dashboard.console.function.report.collect;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import lombok.Data;
+import lombok.Setter;
+
+public class EntityManage {
+
+
+    @Setter
+    private MetaData metaData = new MetaData();
+
+
+    public void supplement(String orId, List<MetaKey> metaKeyList) {
+        MetaData metaData = this.metaData.getMetaMap().get(orId);
+
+    }
+
+
+
+    @Data
+    public static class MetaData{
+
+
+        private Map<String,MetaData> metaMap = new HashMap<>();
+
+        private Long id;
+    }
+
+
+    public static class ClusterMeta{
+
+        private MetaData runtime = new MetaData();
+
+        private MetaData topic = new MetaData();
+
+        private MetaData group = new MetaData();
+
+
+    }
+
+    public static class MetaKey {
+
+
+        private String metaType;
+
+        private String name;
+
+
+    }
+
+}
