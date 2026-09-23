3a0b1d3f03d4d1b8d8e4490eda00b29a015bfb44
ljh <ljh550662@cainiao.com>
2026-09-23 22:55:05 +0800

    失败的时候写为0，不再跳过，使用3位运算符

--- eventmesh-dashboard-console/src/main/java/org/apache/eventmesh/dashboard/console/function/report/collect/exporter/RocketMQCollect.java
+++ eventmesh-dashboard-console/src/main/java/org/apache/eventmesh/dashboard/console/function/report/collect/exporter/RocketMQCollect.java
@@ -330,17 +330,11 @@ public class RocketMQCollect extends AbstractCollect {
                         Float success = nonnegativeFloat(status.get("consumeOKTPS"));
                         Float failed = nonnegativeFloat(status.get("consumeFailedTPS"));
                         Float elapsed = nonnegativeFloat(status.get("consumeRT"));
-                        if (success == null) {
-                            success = 0f;
-                        }
+                        success = success == null ? 0f : success;
                         RocketMQCollect.this.setData(MAPPER.consumerSuccess(topic, this.group, this.clientId, success));
-                        if (failed == null) {
-                            failed = 0f;
-                        }
+                        failed = failed == null ? 0f : failed;
                         RocketMQCollect.this.setData(MAPPER.consumerFailed(topic, this.group, this.clientId, failed));
-                        if (elapsed == null) {
-                            elapsed = 0f;
-                        }
+                        elapsed = elapsed == null ? 0f : elapsed;
                         RocketMQCollect.this.setData(MAPPER.consumerProcessTime(topic, this.group, this.clientId, elapsed));
                     });
                 }
